ef0c82adab38337e07ee1a7a026c6115c64d633b
renpie <160415319+renpie08@users.noreply.github.com>
2024-08-06 09:45:38 +0200

    Aktualisieren von stats.js

--- stats.js
+++ stats.js
@@ -17,10 +17,10 @@ document.addEventListener('DOMContentLoaded', function () {
         update();
     }
 
-    const serverCount = 9999;
-    const userCount = 1242212;
-    const uptime = 542;
-    const commands = 2241222;
+    const serverCount = 86;
+    const userCount = 3100;
+    const uptime = 120;
+    const commands = 172890;
 
     animateNumber('serverCount', serverCount, 2000);
     animateNumber('userCount', userCount, 3000);
